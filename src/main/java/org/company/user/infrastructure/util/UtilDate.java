642e99cfb7ca050bbb5d3799efa1cc8fb71495c3
pablohv <pablo.hdez.v@outlook.com>
2026-09-17 19:03:06 -0600

    fix: update util class

--- src/main/java/org/company/user/infrastructure/util/UtilDate.java
+++ src/main/java/org/company/user/infrastructure/util/UtilDate.java
@@ -5,14 +5,10 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
-public class UtilDate {
-
-    public UtilDate() {
-        throw new IllegalStateException("Utility class");
-    }
+public final class UtilDate {
 
     public static final DateTimeFormatter DATE_TIME = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-    public static Clock CLOCK_ZONE_MEXICO = Clock.system(ZoneId.of("America/Mexico_City"));
+    public static final Clock CLOCK_ZONE_MEXICO = Clock.system(ZoneId.of("America/Mexico_City"));
 
     public static String getCurrentDate() {
         return LocalDateTime.now(CLOCK_ZONE_MEXICO).format(DATE_TIME);
